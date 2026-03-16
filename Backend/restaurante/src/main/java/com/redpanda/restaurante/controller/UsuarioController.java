82660110be02d010d30c65808bcb0d76a16b4b7d
ikermrtnzq <rosiikerantonio@gmail.com>
2026-03-16 19:02:58 +0100

    Creados controller y repository para usuario

---
+++ Backend/restaurante/src/main/java/com/redpanda/restaurante/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.redpanda.restaurante.controller;
+
+//Importa la entidad Carta para que el controlador sepa a qué tabla de la base de datos se refiere
+import com.redpanda.restaurante.entity.Usuario;
+
+//Importa el repositorio de Carta para que el controlador pueda acceder a los datos de la tabla correspondiente
+import com.redpanda.restaurante.repository.UsuarioRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @PostMapping("/login")
+    public List<Usuario> obteneUsuario(@RequestBody Usuario usuario, String correo, String contrasena){
+        return usuarioRepository.findBy(null, null);
+    }
+
+    
+}
