17d44bc06de594d8ef588141bd26252421d21dbc
ikermrtnzq <rosiikerantonio@gmail.com>
2026-03-30 11:26:47 +0200

    Creadas rutas de creacion y eliminacion de ramens de la carta

--- Backend/restaurante/src/main/java/com/redpanda/restaurante/controller/RamenController.java
+++ Backend/restaurante/src/main/java/com/redpanda/restaurante/controller/RamenController.java
@@ -2,7 +2,7 @@ package com.redpanda.restaurante.controller;
 
 //Importa la entidad Ramen para que el controlador sepa a qué tabla de la base de datos se refiere
 import com.redpanda.restaurante.entity.Ramen;
-
+import com.redpanda.restaurante.entity.Sushi;
 //Importa el repositorio de Ramen para que el controlador pueda acceder a los datos de la tabla correspondiente
 import com.redpanda.restaurante.repository.RamenRepository;
 
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,5 +32,25 @@ public class RamenController {
         return ramenRepository.findAll();
     }
 
+    //Para crear un nuevo sushi y añadirlo a la carta
+    @PostMapping("/ramen/create")
+    public Ramen addRamen(@RequestBody Ramen nuevoRamen) {
+        // El repositorio guarda el ramen y nos devuelve el ramen guardado con su ID generado.
+        return ramenRepository.save(nuevoRamen);
+    }
+
+    //Para borra un sushi de la carta
+    @DeleteMapping("/ramen/delete{id}")
+    public String eliminarRamen(@PathVariable Long id){
+        //Comprobamos si existe un ramen con dicho id
+        if (ramenRepository.existsById(id)) {
+            //Si existe lo eliminamos
+            ramenRepository.deleteById(id);
+            return "Ramen eliminado correctamente: " + id; 
+        }else{
+            //Si no existe devolvemos un mensaje de error
+            return "Error: no existe o no se ha podido eliminar el ramen con id: " + id;
+        }
+    }
     
 }
