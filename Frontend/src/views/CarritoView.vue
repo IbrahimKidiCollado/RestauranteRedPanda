06cd749675070b4a2bb456d863251b35f924d91b
ibriiii <ibrahimcollado8@gmail.com>
2026-04-15 00:29:43 +0200

    Finalizado la vista carrito en caso de que no haya productos.

--- Frontend/src/views/CarritoView.vue
+++ Frontend/src/views/CarritoView.vue
@@ -10,20 +10,28 @@
         <h1 class="">{{ $t('carrito.titulo') }}</h1>
         <p><!-- {{ $conteoProductos }} -->{{ $t('carrito.productos-cantidad') }}</p>
     </div>
+    <div class="info-container">
+        <div class="imagen"><img src="/assets/bolsa-de-la-compra.png" alt="Pedido" /></div>
+        <h1>{{ $t('carrito.carrito-vacio') }}</h1>
+        <p>{{ $t('carrito.propuesta-añadir') }}</p>
+        <button @click="navegar('/tienda')">{{ $t('carrito.ver-menu') }}</button>
+    </div>
 </template>
 <style lang="scss" scoped>
 .carrito-container {
-    padding-top: 50px;
-    padding-left: 30px;
+    position: absolute;
+    top: 150px;
+    left: clamp(2px, 10vw, 300px);
+    z-index: 10;
 
     .enlace-volver {
         display: flex;
         align-items: center;
         gap: 10px;
-
         color: $color-blanco-sucioMas;
         font-weight: 500;
         transition: 200ms;
+        cursor: pointer;
 
         &:hover {
             color: $color-texto-blanco;
@@ -33,10 +41,47 @@
     h1 {
         padding-top: 10px;
         font-size: 40px;
+        color: $color-texto-blanco;
     }
 
     p {
         color: $color-texto-blanco;
+    }
+}
+
+.info-container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+
+    .imagen {
+        border-radius: 50%;
+        background-color: $color-rojo-oscuro;
+        display: inline-block;
+
+        img {
+            padding: 30px;
+            display: block;
+        }
+    }
+
+    h1 {
+        padding-top: 30px;
+        text-align: center;
+    }
+
+    p {
+        color: $color-blanco-sucioMas;
+        font-weight: 500;
+        padding-top: 20px;
+        text-align: center;
+    }
+
+    button {
+        @include boton-rojo();
+        margin-top: 40px;
     }
 }
 </style>
